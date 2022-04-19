754ae533a19cbfb164d37a95111c19cdd36ec673
leseraphina <leseraphina@gmail.com>
2022-04-19 21:52:10 +0900

    effect중간

---
+++ src/effect/effec02.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+function Effect02(){
+  const [count,setCount] = useState(0);
+  React.useEffect(()=>{
+    console.log('effect시작과 업데이트',count)
+  },[]);
+  React.useEffect(()=>{
+    console.log('effect시작과 업데이트',count)
+  },[count]);
+  return (
+    <div>
+      <h1>effect기본시점보기</h1>
+    <p>count 변경보기 :{count} </p>
+    <button onClick={click}>click</button>
+    </div>
+  )
+  function click(){
+    setCount(count + 1)
+  }
+
+}
+
+
+export default Effect02
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Sample01 from './component/sample01'
-import Sample02 from './component/sample02'
-import Sample03 from './component/sample03'
-import Sample04 from './component/sample04'
+// import Effect01 from './effect/effec01'
+// import Effect02 from './effect/effec02'
+import Effect03 from './effect/effect03'
 
 function App(){
   return (
     <div>
-      <Sample01 />
-      <Sample02 />
-      <Sample03 />
-      <Sample04 />
+      <Effect03 />
     </div>
   )
 }
